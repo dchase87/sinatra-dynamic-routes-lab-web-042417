e79a18fe450f738235b010696abbee5cd2e27de1
dchase87 <david.chase4@gmail.com>
2017-05-16 10:08:55 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,50 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/reversename/:name" do
+    @name = params[:name].to_s
+    "#{@name.reverse}"
+  end
+
+  get "/square/:number" do
+    @number = params[:number].to_i
+    squared = @number * @number
+    "#{squared}"
+  end
+
+  get "/say/:number/:phrase" do
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    array = []
+    @number.times {array << @phrase}
+    "#{array.join("\n")}"
+  end
+
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+    @word_array = []
+    @word_array << [params[:word1], params[:word2], params[:word3], params[:word4], params[:word5]]
+    "#{@word_array[0].join(' ')}."
+  end
+
+  get "/:operation/:number1/:number2" do
+    @operation = params[:operation].to_s
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+    case @operation
+    when "add"
+      add = @number1 + @number2
+      "#{add}"
+    when "subtract"
+      subtract = @number1 - @number2
+      "#{subtract}"
+    when "multiply"
+      multiply = @number1 * @number2
+      "#{multiply}"
+    when "divide"
+      divide = @number1 / @number2
+      "#{divide}"
+    end
+  end
+
 end
